18911d20633919065cc12dfcebc1b4a7aad8f33b
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-08-02 14:48:45 +0200

    Update gat2vec.py

--- src/guiltytargets/gat2vec.py
+++ src/guiltytargets/gat2vec.py
@@ -2,12 +2,13 @@
 
 """Wrappers around GAT2VEC functions."""
 
-from GAT2VEC import paths as gat2vec_paths
+from GAT2VEC import parsers as gat2vec_parsers, paths as gat2vec_paths
 from GAT2VEC.evaluation.classification import Classification
 from GAT2VEC.gat2vec import Gat2Vec
 
 __all__ = [
     'gat2vec_paths',
+    'gat2vec_parsers',
     'Classification',
     'Gat2Vec',
 ]
